7cd8f908db7615b3e0b64aee0e0ac9886d046d65
avocado-uwu <107556842+avocado-uwu@users.noreply.github.com>
2022-12-05 12:26:50 +0100

    update all hues lol

---
+++ hungryorcs_hue7/src/Orc.java
@@ -0,0 +1,39 @@
+public class main {
+    public static void main(String[] args) {
+        while (true)
+        {
+            drinking();
+
+            grabRightDagger();
+            grabLeftDagger();
+            
+            eating();
+            
+            releasRightDagger();
+            releasLeftDagger();
+            
+        }
+    }
+
+    private static void releasLeftDagger() {
+    }
+
+    private static void releasRightDagger() {
+        
+    }
+
+    private static void grabLeftDagger() {
+        
+    }
+
+    private static void grabRightDagger() {
+        
+    }
+
+    private static void drinking() {
+    }
+
+    private static void eating() {
+        
+    }
+}
